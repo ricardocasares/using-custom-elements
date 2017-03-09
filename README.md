3fe9815dd87b3f706e93b2ad944297f587fc8d40
Ricardo Casares <ricardocasares@gmail.com>
2017-03-09 21:52:59 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# using-custom-elements
+Start using Custom Elements
+
+## Demo
+Visit [elements.analogic.al](https://elements.analogic.al)
---
+++ index.js
@@ -0,0 +1,259 @@
+
+// BASE
+
+class BaseElement extends HTMLElement {
+  constructor () {
+    super()
+
+    this.connected = false
+  }
+
+  onConnect () {
+    // to be overriden by user
+  }
+
+  renderCallback () {
+    // to be overriden by user
+  }
+
+  connectedCallback () {
+    if (!this.connected) {
+      this.onConnect()
+      this.connected = true
+    }
+
+    this.renderCallback()
+  }
+
+  attributeChangedCallback (attr, prev, curr) {
+    if (this.connected) {
+      this.renderCallback()
+    }
+  }
+}
+
+// DECK
+
+class Deck extends BaseElement {
+  constructor () {
+    super()
+
+    this.slides = []
+    this.timer = null
+    this.current = null
+  }
+
+  static get observedAttributes () {
+    return ['auto', 'delay', 'active']
+  }
+
+  get active () {
+    return parseInt(window.location.hash.replace('#', ''), 10) || 0
+  }
+
+  set active (idx) {
+    window.location.hash = `${idx}`
+    this.setAttribute('active', `${idx}`)
+  }
+
+  get auto () {
+    return this.hasAttribute('auto')
+  }
+
+  set auto (value) {
+    value ? this.setAttribute('auto', ``)
+          : this.removeAttribute('auto')
+  }
+
+  get interval () {
+    return parseFloat(this.getAttribute('interval') || 5)
+  }
+
+  set interval (value) {
+    value ? this.setAttribute('interval', `${value}`)
+          : this.removeAttribute('interval')
+  }
+
+  onConnect () {
+    this.collectSlides()
+    this.attachNavigation()
+  }
+
+  renderCallback () {
+    this.autoMode()
+    setTimeout(this.activateSlide.bind(this, this.active), 0)
+  }
+
+  collectSlides () {
+    this.slides = Array.from(this.querySelectorAll('x-slide'))
+  }
+
+  attachNavigation () {
+    document.addEventListener('click', this.next.bind(this))
+    document.addEventListener('keydown', this.handleKeydown.bind(this))
+  }
+
+  autoMode () {
+    if (this.auto) {
+      clearInterval(this.timer)
+      this.timer = setInterval(this.next.bind(this), this.interval * 1000)
+    }
+  }
+
+  activateSlide (idx) {
+    if (this.current) {
+      this.current.active = false
+    }
+
+    this.current = this.slides[idx]
+    this.current.active = true
+    this.active = idx
+  }
+
+  handleKeydown (e) {
+    switch (e.keyCode) {
+      case 32:
+        this.next()
+        return
+      case 37:
+        this.prev()
+        return
+      case 39:
+        this.next()
+    }
+  }
+
+  next () {
+    let next = this.active + 1
+    let total = this.slides.length - 1
+
+    if (next <= total) {
+      this.activateSlide(next)
+    } else {
+      this.activateSlide(0)
+    }
+  }
+
+  prev () {
+    let prev = this.active - 1
+
+    if (prev >= 0) {
+      this.activateSlide(prev)
+    } else {
+      this.activateSlide(this.slides.length - 1)
+    }
+  }
+
+  disconnectedCallback () {
+    document.removeEventListener('click', this.next)
+    document.removeEventListener('keydown', this.handleKeydown)
+  }
+}
+
+// SLIDE
+
+const DEFAULT_IN = 'fadeIn'
+const DEFAULT_OUT = 'fadeOut'
+
+class Slide extends BaseElement {
+  static get observedAttributes () {
+    return ['active', 'in', 'out']
+  }
+
+  get active () {
+    return this.hasAttribute('active')
+  }
+
+  set active (value) {
+    value ? this.setAttribute('active', '')
+            : this.removeAttribute('active')
+  }
+
+  get in () {
+    return this.getAttribute('in') || DEFAULT_IN
+  }
+
+  get out () {
+    return this.getAttribute('out') || DEFAULT_OUT
+  }
+
+  renderCallback () {
+    this.attachStyles()
+  }
+
+  attachStyles () {
+    this.style.cssText = `
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+      `
+    this.classList.add('animated')
+
+    if (this.active) {
+      this.classList.add(this.in)
+      this.classList.remove(this.out)
+    } else {
+      this.classList.add(this.out)
+      this.classList.remove(this.in)
+    }
+  }
+}
+
+// ASCII ICON
+
+class AsciiFace extends BaseElement {
+  static get observedAttributes () {
+    return ['face', 'size', 'color']
+  }
+
+  get face () {
+    return this.getAttribute('face') || 'meh'
+  }
+
+  get color () {
+    return this.getAttribute('color') || 'currentColor'
+  }
+
+  get size () {
+    return parseFloat(this.getAttribute('size'))
+  }
+
+  constructor () {
+    super()
+
+    this.faces = {
+      disapprove: 'ಠ_ಠ',
+      shrug: '¯\\_(ツ)_/¯',
+      meh: '¯\\(°_o)/¯',
+      love: '♥‿♥',
+      careless: '◔_◔',
+      dance: 'ヾ(-_- )ゞ',
+      cry: 'ಥ﹏ಥ',
+      happy: 'ヽ(´▽`)/',
+      bear: 'ʕ•ᴥ•ʔ',
+      what: '¯\\_(⊙︿⊙)_/¯'
+    }
+  }
+
+  renderCallback () {
+    this.innerHTML = this.faces[this.face]
+    this.style.cssText = this.buildStyles()
+  }
+
+  buildStyles () {
+    let styles = []
+
+    if (this.color) styles.push(`color: ${this.color};`)
+    if (this.size) styles.push(`font-size: ${this.size}px;`)
+
+    return styles.join(' ')
+  }
+}
+
+// DEFINITION
+
+customElements.define('x-deck', Deck)
+customElements.define('x-slide', Slide)
+customElements.define('x-ascii', AsciiFace)
