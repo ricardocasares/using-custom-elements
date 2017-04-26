8e9ba7625f5694bc207aca0443a26539c5a3d59a
Ricardo Casares <ricardocasares@gmail.com>
2017-04-26 12:38:03 +0200

    Simplify rolling slide

--- src/RollingSlide.js
+++ src/RollingSlide.js
@@ -1,21 +1,10 @@
-const DEFAULT_IN_ROLL = 'rollIn'
-const DEFAULT_OUT_ROLL = 'rollOut'
-
 class RollingSlide extends Slide {
   constructor () {
     super()
   }
 
-  get in () {
-    return this.getAttribute('in') || DEFAULT_IN_ROLL
-  }
-
-  get out () {
-    return this.getAttribute('out') || DEFAULT_OUT_ROLL
-  }
-
   inlineStyles () {
-      return Object.assign(super.inlineCSS(), {
+      return Object.assign(super.inlineStyles(), {
           textAlign: 'center',
           borderColor: 'black',
           backgroundColor: 'white',
